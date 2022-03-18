dfe9b152d200daab11c6600a5de04079840ddd29
DutchTechie <rkock127@gmail.com>
2022-03-18 12:56:09 +0100

    Initial Commit

---
+++ src/main/java/com/springproject/triviaapprk22/TriviaAppRk22Application.java
@@ -0,0 +1,13 @@
+package com.springproject.triviaapprk22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaAppRk22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaAppRk22Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springproject/triviaapprk22/TriviaAppRk22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.triviaapprk22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaAppRk22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
